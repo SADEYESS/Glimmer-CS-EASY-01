86a9bc3623b8980b83be58ac6718a22264a998d4
SADEYE <weihguan@outlook.com>
2024-09-23 13:15:38 +0800

    WORK01

--- GongYueShu.c
+++ GongYueShu.c
@@ -4,13 +4,13 @@ int main()
 {
     int m,n,GCD;   //声明需要输入的两个变量m，n，由于0<m,n<2^31,可以用int类型表示
                    //声明一个变量GCD作为最大公约数(Greatest Common Divisor)来进行后面的运算
-    while(1)       //使用 while 循环验证用户的输入是否满足要求，满足要求则进入运算
+    while(1)       //使用 while 循环验证用户的输入是否满足要求
     {
     printf("Please enter two positive integers:\n");  //提示用户输入两个正整数
     scanf("%d %d",&m,&n); //读取用户输入
         if (m > 0 && m < 2147483648 && n > 0 && n < 2147483648) //m，n要同时满足>0且<2^31
         {
-            break; //满足条件则跳出循环
+            break; //满足条件则跳出循环，进入下一步的运算
         }
         else
         {
